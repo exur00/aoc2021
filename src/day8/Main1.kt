f74f2e66bfa26a9c2e7f286f27d484fec2d98839
loekarogge <loeka.rogge@gmail.com>
2021-12-08 15:50:25 +0100

    day6 Main1 working

--- src/day8/Main1.kt
+++ src/day8/Main1.kt
@@ -3,37 +3,18 @@ package day8
 import java.io.File
 
 fun main() {
-    val a = File("src/day8/input.txt").readLines()
-    val allChars = setOf('a', 'b', 'c', 'd', 'e', 'f', 'g')
+    var result = 0
+    val lengthSet = setOf(2, 3, 4, 7)
 
+    val a = File("src/day8/input.txt").readLines()
     for (tmp in a) {
         val b = tmp.split("|")
-        val pos: HashMap<Char, Set<Char>> = HashMap()
-        for (i in allChars){
-            pos.put(i, hashSetOf('a', 'b', 'c', 'd', 'e', 'f', 'g'))
-        }
-        val c = b.get(0).split(" ")
+        val c = b.get(1).split(" ")
         for (i in c){
-            if (i.length == 2) {
-                canBe(i, setOf('f', 'g'), pos)
+            if (lengthSet.contains(i.length)) {
+                result++
             }
         }
-        print("biep")
-
-
     }
-
+    print(result)
 }
-
-fun canBe (first: String, second: Set<Char>, map: HashMap<Char, Set<Char>>, allChars: Set<Char>) {
-    for (ch in allChars) {
-        if (ch in first) {map.put(ch, map.get(ch)!!.intersect(second))}
-        else {map.get(ch)!!.}
-    }
-
-}
-
-fun canOnlyBe (first: Set<Char>, second: Set<Char>, map: HashMap<Char, Set<Char>>) {
-
-}
-
